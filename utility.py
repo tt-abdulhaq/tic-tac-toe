a151e2e282e22e362d5fba133eb439e6a99df8d9
tt-abdulhaq <abdularifi10am135@gmail.com>
2023-12-11 22:56:56 +0500

    write check winner function

--- utility.py
+++ utility.py
@@ -1,4 +1,6 @@
 import random 
+import sys
+
 
 
 
@@ -8,6 +10,21 @@ board_list = [
         ['|',  ' ', '|', ' ', '|', ' ', '|'],
         ['|',  ' ', '|', ' ', '|', ' ', '|']
     ]
+
+winner_list = sorted([
+    [(0,1), (0,3), (0,5)],
+    [(1,1), (1,3), (1,5)],
+    [(2,1), (2,3), (2,5)],
+    [(0,1), (1,1), (2,1)],
+    [(0,3), (1,3), (2,3)],
+    [(0,5), (1,5), (2,5)],
+    [(0,1), (1,3), (2,5)]
+])
+
+
+user_fill_cell = []
+computer_fill_cell = []
+
 
 
 def print_board(board_list = board_list):
@@ -52,17 +69,42 @@ def get_board_list(board_list, row, column, value):
     board_list[row][column] = value
     return board_list
 
-#TODO: write a function to check the end of game and specifie the wener.
-for _ in range(4):
+def check_winner(filled_cells, player, winner_list = winner_list):
+    if sorted(filled_cells) in winner_list:
+        if player == 'user':
+            return (1, 'user')
+        else:
+            return (1, "computer")
+    else:
+        return (0, None)
+
+for _ in range(3):
     print_board()
     row, column, value = get_user_input()
     while board_list[row][column] in ['X', 'O']:
         row, column, value = get_user_input()
+    user_fill_cell.append((row, column))
     board_list = get_board_list(board_list=board_list, row=row, column=column, value=value)
     row, column, value = get_computer_input(board_list)
+    computer_fill_cell.append((row, column))
+    winner_status, winner = check_winner(computer_fill_cell, 'computer')
     board_list = get_board_list(board_list=board_list, row=row, column=column, value=value)
+
+
 print_board()
-print("Finished the game")
+winner_status, winner = check_winner(user_fill_cell, 'user')
+if winner_status:
+    print(f"Your are win Congertulation. ):")
+    sys.exit()
+    
+
+winner_status, winner = check_winner(computer_fill_cell, 'computer')
+if winner_status:
+    print("computer is the winner. sorry! :(")
+    sys.exit()
+
+print("Finished the game, no one is winner do want play again!")
+sys.exit()
     
 
     
